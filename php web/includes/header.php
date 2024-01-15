bb197006059916c12b4addf06253d6e7d217f114
mariah bugeja <mariahbugeja16@gmail.com>
2024-01-15 16:18:52 +0100

    transafred my html into php
    Made a folder for my html file
    created a header
    css
    and asstes folder
    In the google created a database for products

---
+++ php web/includes/header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.rtl.min.css" integrity="sha384-DOXMLfHhQkvFFp+rWTZwVlPVqdIhpDVYT9csOnHSgWQWPX0v5MCGtjCJbY6ERspU" crossorigin="anonymous">
+</head>
+<body>
+   
+    <nav class="navbar navbar-expand-lg custom-navbar">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="assets/logo.png" alt="Your Logo" class="custom-logo">
+            </a>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav" style="margin-left: auto;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="meal.php">Meal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Customize your own</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+
+</body>
+</html>
---
+++ php web/index.php
@@ -0,0 +1,105 @@
+<?php
+include 'includes/header.php';
+?>
+<html>
+    <head>
+    <link rel="stylesheet" href="style.css"> 
+</head>
+    <body>
+ <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://healthydrivenchicago.com/wp-content/uploads/2019/12/AtHomeMealPlan_Web.jpg" class="d-block w-100" alt="Image 1">
+            </div>
+            <div class="carousel-item">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ79XnFQojb453BK4_aICPRq6Okp1vD-Azk0u5hY8pHwsQyToPpoT54Qp_PLYp_l8JnYeY&usqp=CAU" class="d-block w-100" alt="Image 2">
+            </div>
+            <div class="carousel-item">
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.heart.org%2Fen%2Fhealthy-living%2Fhealthy-eating%2Fcooking-skills%2Fmeal-planning%2Fmeal-planning-benefits--how-tos-of-family-dinners&psig=AOvVaw3JDC3qsDerIr3bdeZAud8S&ust=1705413286413000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCYqNvF34MDFQAAAAAdAAAAABAD" class="d-block w-100" alt="Image 3">
+            </div>
+            
+        </div>
+    </div>
+    <section class="colored-background">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>All ingredients used by chef are local, fresh, contains no GMOs, no additives, no trans-fat. Like that, we ensure that the finest quality is provided in order to build healthy minds, bodies & habits!</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section class="image-text-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="assets/healthy.png" alt="Image" class="img-fluid">
+                </div>
+                <div class="col-md-6">
+                    <div class="text-content">
+                        <h2>Discover Our Healthy Meals</h2>
+                        <p class="MIDDLETEXT">Choose from a variety of delicious and nutritious meals prepared with fresh ingredients. We offer the best in healthy eating options that cater to your taste and preferences.</p>
+                        <a href="shopping.html" class="rectangular-button">View Menu</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="background-image-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center textcolour">
+                    <h2>Chef Oink: Savoring the Flavor of Health, One Bite at a Time!</h2>
+                    <section class="image-text-section">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="card">
+                                        <img src="assets/kid1.png" alt="Image 1" class="card-img-top">
+                                        <div class="card-body">
+                                            <h5 class="card-title card-title-common">Chef's Specials</h5>
+                                            <p class="card-text">
+                                                Explore Chef Oink's exclusive dishes crafted with love and expertise. Our culinary team prepares unique, nutritious meals that your kids will love. Don't miss out on these special creations!
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="card">
+                                        <img src="assets/kid2.png" alt="Image 2" class="card-img-top">
+                                        <div class="card-body">
+                                            <h5 class="card-title card-title-common">Dietary Options</h5>
+                                            <p class="card-text">
+                                                We understand that every child is unique. Chef Oink offers a variety of dietary options, including vegetarian, gluten-free, and allergen-free meals. You can rest easy knowing your child's dietary needs are our priority.
+                                                !</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="card">
+                                        <img src="assets/kid3.png" alt="Image 2" class="card-img-top">
+                                        <div class="card-body">
+                                            <p  class="card-title card-title-common">Create Your Child's Food Adventure </p>
+                                            <p class="card-text">
+                                                At Chef Oink's, customize your child's meal to perfection! From spice levels to unique combinations, we've got it covered. Let us know your preferences, and we'll make dining a delight for your little ones!</p>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    
+                </div>
+            </div>
+        </div>
+      
+
+        
+    </section>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
